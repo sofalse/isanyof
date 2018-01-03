b05b89367cb70e7b573c311bed589c786994c914
sofalse <j989898@gmail.com>
2018-01-03 20:43:58 +0100

    Initial commit

---
+++ isanyof/index.js
@@ -0,0 +1,51 @@
+﻿/* *****************
+ *
+ * isAnyOf module by Michal W.
+ * Licensed under GNU GPL License v3
+ *
+ **************** */
+
+Boolean.prototype.isanyof = function (...args) {
+    for (arg of args) {
+        if (this == arg) {
+            return true
+        }
+    }
+    return false
+}
+
+Number.prototype.isanyof = function (...args) {
+    for (arg of args) {
+        if (this == arg) {
+            return true
+        }
+    }
+    return false
+}
+
+String.prototype.isanyof = function (...args) {
+    for (arg of args) {
+        if (this == arg) {
+            return true
+        }
+    }
+    return false
+}
+
+Symbol.prototype.isanyof = function (...args) {
+    for (arg of args) {
+        if (this == arg) {
+            return true
+        }
+    }
+    return false
+}
+
+Object.prototype.isanyof = function (...args) {
+    for (arg of args) {
+        if (this == arg) {
+            return true
+        }
+    }
+    return false
+}
---
+++ isanyof/test.js
@@ -0,0 +1,9 @@
+﻿require('./index.js')
+
+var bool = true
+var number = 2
+
+console.log(bool.isanyof(true, true, false))
+console.log(bool.isanyof(false, false))
+console.log(number.isanyof(5, 1, true))
+console.log(number.isanyof(5, 1, 2))
